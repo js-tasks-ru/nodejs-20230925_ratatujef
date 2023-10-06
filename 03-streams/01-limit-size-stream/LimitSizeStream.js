8f6976bef444eb154c461e39c7e29d406b066df3
Ivan Glebov <ratatujef@gmail.com>
2023-10-06 18:01:14 +0200

    limited stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this._limit= options.limit;
+    this._storage=0;
   }
 
-  _transform(chunk, encoding, callback) {
+  _transform(chunk, encocde, callback) {
+    this._storage += chunk.byteLength;
 
+    if (this._storage <= this._limit) {
+      callback(null, chunk);
+    } else {
+      this.emit('error', new LimitExceededError());
+    }
   }
 }
 
