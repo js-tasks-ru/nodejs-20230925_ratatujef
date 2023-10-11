03122c6310622383decb9dae639a5d7215390776
Ivan Glebov <ratatujef@gmail.com>
2023-10-11 16:30:56 +0200

    streams

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,8 +1,11 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+const MEGIBITE = 1000000;
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -12,7 +15,45 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        res.statusCode=400;
+        res.end();
+        return;
+      }
 
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end();
+        return;
+      }
+      const limitedStream = new LimitSizeStream({limit: MEGIBITE});
+      const writeStream = fs.createWriteStream(filepath);
+
+      limitedStream.on('error', ()=>{
+        fs.unlink(filepath, (err) => {
+          if (err) throw err;
+
+          res.statusCode = 413;
+          res.end();
+          return;
+        });
+      });
+      req.pipe(limitedStream).pipe(writeStream).on('end', ()=>{
+        res.statusCode = 201;
+        res.end();
+      });
+
+      req.on('close', ()=>{
+        limitedStream.destroy();
+
+        fs.unlink(filepath, (err) => {
+          if (err) throw err;
+
+          res.statusCode = 500;
+          res.end();
+          return;
+        });
+      });
       break;
 
     default:
