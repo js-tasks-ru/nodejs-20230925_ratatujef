6230c1db34d9d511de0b7645bf1ba06af7b76ae8
Ivan Glebov <ratatujef@gmail.com>
2023-11-03 17:47:16 +0100

    06/02-api

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,31 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const {subcategory} = ctx.query;
+  const { subcategory } = ctx.query;
 
   if (!subcategory) return next();
-
-  ctx.body = {};
+  const products = await Product.find({ subcategory: subcategory })
+  ctx.body = { products: products.map(mapProduct) };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find({});
+  if (!products.length) ctx.throw(404, 'Not Found');
+
+  ctx.body = { products: products.map(mapProduct) };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+
+  if (!mongoose.isValidObjectId(ctx.params.id)) ctx.throw(400, 'invalid ID');
+
+  const product = await Product.findById(ctx.params.id);
+  if (!product) {
+    ctx.throw(404, 'not found');
+  }
+
+  ctx.body = { product: mapProduct(product) };
 };
 
